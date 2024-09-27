0574f67a6f5c7323290875caa3f440f4a3481d85
Lucas Salvatori <lsalvatori.hms@gmail.com>
2024-09-26 21:47:33 -0300

    Agregado filtrado y modificacion de datos de objetos en array

--- objetos.js
+++ objetos.js
@@ -49,3 +49,74 @@ function devuelveObjeto() {
     let  per = { nombre: "fff", edad: 55}   
     return per
 }
+
+
+const persona4 = {
+    nombre: "Ludmila",
+    edad: 18,
+    esFumador: false
+}
+
+const persona5 = {
+    nombre: "Roberto",
+    edad: 45,
+    esFumador: true
+}
+
+personas.push(persona4);
+personas.push(persona5);
+console.log(personas);
+
+
+// Realiza una funcion que devuelva las personas mayores a 30 años
+
+// Opcion 1 con funcion normal
+function mayoresDe30_1(edadesIn) {
+    return edadesIn.filter(revisarMax30);
+
+    function revisarMax30(edad) {
+        return edad > 30;
+    }
+}
+
+// Opcion 2 con funcion anonima
+function mayoresDe30_2(edadesIn) {
+    return edadesIn.filter(
+        function (edad) {
+            return edad > 30;
+        }
+    );
+}
+
+// Opcion 3 con funcion flecha
+function mayoresDe30_3(edadesIn) {
+    return edadesIn.filter(edad => edad > 30);
+}
+
+const edades = [20, 35, 80, 44, 15];
+// console.log(mayoresDe30_1(edades));
+// console.log(mayoresDe30_2(edades));
+// console.log(mayoresDe30_3(edades));
+
+// mayores de 30 utilizando objetos
+function mayoresDe30_Obj(personasIn) {
+    return personasIn.filter(persona => persona.edad > 30);
+}
+
+console.log(mayoresDe30_Obj(personas));
+
+
+// personas[2].edad = 66;
+// console.log(personas);
+
+// Cambiar edad
+function cambiarEdad(personasIn, nombre) {
+    personasIn.forEach(persona => {
+        if (persona.nombre === nombre) {
+            persona.edad = 66;
+        }
+    })
+}
+
+cambiarEdad(personas, 'Ludmila')
+console.log(personas);
